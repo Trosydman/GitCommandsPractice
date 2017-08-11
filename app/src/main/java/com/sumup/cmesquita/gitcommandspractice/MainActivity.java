92951e23030ea10e1dd050a39ff89ea3fa81c9d3
Carlos <carlos.mesquita@sumup.com>
2017-08-11 14:37:25 +0200

    Create attribute Button

--- app/src/main/java/com/sumup/cmesquita/gitcommandspractice/MainActivity.java
+++ app/src/main/java/com/sumup/cmesquita/gitcommandspractice/MainActivity.java
@@ -2,8 +2,11 @@ package com.sumup.cmesquita.gitcommandspractice;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnFinish;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
