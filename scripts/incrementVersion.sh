571af7070c1b977567b17f9cb3de673c3906b309
Carlos <carlos.mesquita@sumup.com>
2017-12-07 15:02:50 +0100

    Redesign for last time the incrementVersion script

--- scripts/incrementVersion.sh
+++ scripts/incrementVersion.sh
@@ -4,20 +4,19 @@ set -e
 set -o pipefail
 set -u
 
-if [ $# -eq 0 ]
-  then
-    echo "Usage : ./incrementVersion.sh ('App' or 'SDK')"
+if [[ $# -eq 0 ]];then
+    echo 'Usage : ./incrementVersion.sh ("App" or "SDK")'
     exit 1
 fi
 
+# $1 should be or 'App' or 'SDK'
 PROJECT=$1
 MAIN_FILE='./build.gradle'
-VERSIONNAME=""
 
-if [ "$PROJECT" == "App" ];then
+if [[ "$PROJECT" == "App" ]];then
     # versionApp = "1.83.0"
     VERSIONNAME=$(sed -n /'versionApp = "[0-9\.]*".*'/p $MAIN_FILE)
-elif [ "$PROJECT" == "SDK" ];then
+elif [[ "$PROJECT" == "SDK" ]];then
     # versionSdk = "2.4.0"
     VERSIONNAME=$(sed -n /'versionSdk = "[0-9\.]*".*'/p $MAIN_FILE)
 else
@@ -26,23 +25,30 @@ else
 fi
 
 # Picking the versionName by removing unnecessary things
+# [versionXYZ = "]
 VERSIONNAME=${VERSIONNAME%\"*}
+# ["]
 VERSIONNAME=${VERSIONNAME##*\"}
 
 if [[ ${#VERSIONNAME} -eq 0 ]];then
-  echo "Failed to extract VERSIONNAME"
-  exit 1
+    echo "Failed to extract VERSIONNAME"
+    exit 1
 fi
 
 echo "Current $PROJECT version: $VERSIONNAME"
 echo
 read -r -p "New version: " NEW_VERSIONNAME
 if [[ ${#NEW_VERSIONNAME} -eq 0 ]];then
-  echo "ERROR: Empty $PROJECT version"
-  exit 1
+    echo "ERROR: Empty $PROJECT version"
+    exit 1
 fi
 
-if [ "$PROJECT" == "App" ];then
+if ! git diff --quiet ;then
+    echo "ERROR: You have changes, make sure your working copy is clean and run again."
+    exit 1
+fi
+
+if [[ "$PROJECT" == "App" ]];then
     sed -i '' /'.versionApp = "[0-9\.]*".*'/s/'"[0-9\.]*"'/\"$NEW_VERSIONNAME\"/ "$MAIN_FILE"
 else
     sed -i '' /'.versionSdk = "[0-9\.]*".*'/s/'"[0-9\.]*"'/\"$NEW_VERSIONNAME\"/ "$MAIN_FILE"
@@ -55,14 +61,12 @@ echo "Updating version..."
 #================================== Commit ==================================
 git add "$MAIN_FILE"
 git commit -m "Iterate for $PROJECT release $NEW_VERSIONNAME"
-git diff --quiet
 echo
 read -r -p "Increment committed. Push to origin (y/n)? " REPLY
-if [ "$REPLY" == "y" ]
-then
- git push
- echo
- echo "New $PROJECT version pushed!"
+if [[ "$REPLY" == "y" ]];then
+    git push
+    echo
+    echo "New $PROJECT version pushed!"
 fi
 #============================================================================
 
